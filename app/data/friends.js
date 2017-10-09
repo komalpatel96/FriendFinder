4381fa083aa6b2be73141af97e93a1d30086d200
komalpatel96 <komalpatel96@gmail.com>
2017-10-09 01:21:40 -0400

    created folders and their related files

---
+++ app/data/friends.js
@@ -0,0 +1,96 @@
+{
+  "name":"Joey Tribiani",
+  "photo":"https://typeset-beta.imgix.net/rehost%2F2016%2F9%2F13%2F448e1dea-fcbc-4f59-acab-25946e3e47ac.jpg",
+  "scores":[
+      5, //1
+      3, //2
+      1, //3
+      1, //4
+      4, //5
+      5, //6
+      3, //7
+      1, //8
+      5, //9
+      5 //10
+    ]
+},
+{
+  "name":"Monica Geller",
+  "photo":"http://images.huffingtonpost.com/2015-01-13-9FEMCHEF-thumb",
+  "scores":[
+      5, //1 
+      5, //2
+      1, //3
+      5, //4
+      4, //5
+      1, //6
+      5, //7
+      1, //8
+      3, //9
+      5 //10
+    ]
+},
+{
+  "name":"Chandler Bing",
+  "photo":"http://images6.fanpop.com/image/photos/32000000/Chandler-chandler-bing-32087437-500-556.jpg",
+  "scores":[
+      1, //1
+      3, //2
+      1, //3
+      5, //4
+      1, //5
+      5, //6
+      1, //7
+      3, //8
+      2, //9
+      5 //10
+    ]
+},
+{
+  "name":"Ross Geller",
+  "photo":"http://i1.ytimg.com/vi/rPi9258zmh8/0.jpg",
+  "scores":[
+      5, //1
+      5, //2
+      5, //3
+      1, //4
+      4, //5
+      4, //6
+      5, //7
+      5, //8
+      1, //9
+      3 //10
+    ]
+},
+{
+  "name":"Phoebe Buffay",
+  "photo":"https://lovelace-media.imgix.net/uploads/501/ead17a00-b144-0132-4672-0e9062a7590a.jpg?w=740&h=555&fit=crop&crop=faces&auto=format&q=70",
+  "scores":[
+      5, //1
+      1, //2
+      2, //3
+      1, //4
+      5, //5
+      5, //6
+      1, //7
+      1, //8
+      5, //9
+      4 //10
+    ]
+},
+{
+  "name":"Rachel Green",
+  "photo":"http://data.whicdn.com/images/245510520/superthumb.jpg",
+  "scores":[
+      2, //1
+      4, //2
+      4, //3
+      3, //4
+      3, //5
+      3, //6
+      3, //7
+      2, //8
+      5, //9
+      5 //10
+    ]
+}
